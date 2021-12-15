c4a1faacb71e58431aa96ab420d4133d3cdfd515
Batuhan Birkan <batuhanbirkan6@gmail.com>
2021-12-15 20:54:27 +0300

    reservation components

---
+++ pages/reservations.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="main-wrapper">
+    <v-card>
+      <v-simple-table>
+        <template #default>
+          <thead>
+            <tr>
+              <th class="text-left">Name</th>
+              <th class="text-left">Calories</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in desserts" :key="item.name">
+              <td>{{ item.name }}</td>
+              <td>{{ item.calories }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      pasword: '',
+      desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+        ],
+    }
+  },
+}
+</script>
+<style scoped>
+.main-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.filter-panel {
+  padding: 50px 30px;
+  width: 320px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  border-radius: 13px;
+  box-shadow: 0 0 20px 4px rgba(0, 0, 0, 0.3);
+}
+
+.form-inputs {
+  margin-top: 40px;
+}
+
+.login-button {
+  margin-top: 30px;
+}
+</style>
